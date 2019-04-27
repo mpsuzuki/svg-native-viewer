1ad295e570dfc8b0ec25af1b814cec2eec5f5e13
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2019-04-27 02:39:30 +0000

    wip: enable to emit SVG, PNG, XML (CairoScript is used as fallback)

--- svgnative/example/testCairo/TestCairo.cpp
+++ svgnative/example/testCairo/TestCairo.cpp
@@ -14,11 +14,21 @@ governing permissions and limitations under the License.
 #include <list>
 #include "CairoSVGRenderer.h"
 #include "cairo.h"
+
+#if CAIRO_HAS_SCRIPT_SURFACE
 #include "cairo-script.h"
+#endif
+#if CAIRO_HAS_SVG_SURFACE
+#include "cairo-svg.h"
+#endif
+#if CAIRO_HAS_XML_SURFACE
+#include "cairo-xml.h"
+#endif
 
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cctype>
 
 int main(int argc, char* const argv[])
 {
@@ -47,14 +57,60 @@ int main(int argc, char* const argv[])
     docExtents.width = doc->Width();
     docExtents.height = doc->Height();
 
-    auto cairoSurface = cairo_recording_surface_create( CAIRO_CONTENT_COLOR_ALPHA, &docExtents );
+    std::string outPath = argv[2];
+    std::string suffix = outPath.substr(outPath.rfind('.') + 1).c_str();
+    std::transform(suffix.begin(), suffix.end(), suffix.begin(), ::tolower);
+
+    cairo_surface_t* cairoSurface;
+#if CAIRO_HAS_SVG_SURFACE
+    if (suffix == "svg")
+    {
+        cairoSurface = cairo_svg_surface_create(argv[2], doc->Width(), doc->Height());
+        cairo_svg_surface_set_document_unit(cairoSurface, CAIRO_SVG_UNIT_PX);
+    }
+    else
+#endif
+        cairoSurface = cairo_recording_surface_create( CAIRO_CONTENT_COLOR_ALPHA, &docExtents );
+
     auto cairoContext = cairo_create( cairoSurface );
 
     renderer->SetCairo( cairoContext );
     doc->Render();
 
-    auto cairo_dev = cairo_script_create(argv[2]);
-    cairo_script_from_recording_surface(cairo_dev, cairoSurface);
+#if CAIRO_HAS_SVG_SURFACE
+    if (suffix == "svg")
+    {
+        cairo_show_page( cairoContext );
+        cairo_destroy( cairoContext );
+        cairo_surface_destroy( cairoSurface );
+        // auto cairo_surface_svg = cairo_svg_surface_create(argv[2], doc->Width(), doc->Height());
+        // auto cairo_svg = cairo_create( cairo_surface_svg );
+        // cairo_set_source_surface( cairo_svg, cairoSurface, 0, 0 );
+        // cairo_paint( cairo_svg );
+        // cairo_destroy( cairo_svg );
+        // cairo_surface_destroy( cairo_svg );
+    }
+    else
+#endif
+#if CAIRO_HAS_XML_SURFACE
+    if (suffix == "xml")
+    {
+        auto cairo_dev = cairo_xml_create(argv[2]);
+        cairo_xml_for_recording_surface(cairo_dev, cairoSurface);
+    }
+    else
+#endif
+#ifndef CAIRO_HAS_SCRIPT_SURFACE
+        cairo_surface_write_to_png( cairoSurface, argv[2] );   
+#else
+    if (suffix == "png")
+        cairo_surface_write_to_png( cairoSurface, argv[2] );   
+    else
+    {
+        auto cairo_dev = cairo_script_create(argv[2]);
+        cairo_script_from_recording_surface(cairo_dev, cairoSurface);
+    }
+#endif
     
     return 0;
 }
