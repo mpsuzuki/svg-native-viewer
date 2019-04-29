390382f143fa712b39aafb1d499e1cca4ab79219
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2019-04-29 11:38:34 +0000

    TestCairo: take more arguments to test single source multiple rendering cases

--- svgnative/example/testCairo/TestCairo.cpp
+++ svgnative/example/testCairo/TestCairo.cpp
@@ -32,7 +32,7 @@ governing permissions and limitations under the License.
 
 int main(int argc, char* const argv[])
 {
-    if (argc != 3)
+    if (argc < 3)
     {
         std::cout << "Incorrect number of arguments." << std::endl;
         return 0;
@@ -57,53 +57,65 @@ int main(int argc, char* const argv[])
     docExtents.width = doc->Width();
     docExtents.height = doc->Height();
 
-    std::string outPath = argv[2];
-    std::string suffix = outPath.substr(outPath.rfind('.') + 1).c_str();
-    std::transform(suffix.begin(), suffix.end(), suffix.begin(), ::tolower);
+    for (int i = 2; i < argc; i++ )
+    {
+        std::string outPath = argv[i];
+        std::string suffix = outPath.substr(outPath.rfind('.') + 1).c_str();
+        std::transform(suffix.begin(), suffix.end(), suffix.begin(), ::tolower);
 
-    cairo_surface_t* cairoSurface = cairo_recording_surface_create( CAIRO_CONTENT_COLOR_ALPHA, &docExtents );
+        cairo_surface_t* cairoSurface = cairo_recording_surface_create( CAIRO_CONTENT_COLOR_ALPHA, &docExtents );
 
-    auto cairoContext = cairo_create( cairoSurface );
+        auto cairoContext = cairo_create( cairoSurface );
 
-    renderer->SetCairo( cairoContext );
-    doc->Render();
+        renderer->SetCairo( cairoContext );
+        doc->Render();
 
 #if CAIRO_HAS_SVG_SURFACE
-    if (suffix == "svg")
-    {
-        auto cairo_surface_svg = cairo_svg_surface_create(argv[2], doc->Width(), doc->Height());
-        auto cairo_svg = cairo_create( cairo_surface_svg );
-        cairo_set_source_surface( cairo_svg, cairoSurface, 0, 0 );
-        cairo_paint( cairo_svg );
-        cairo_show_page( cairo_svg );
-        cairo_destroy( cairo_svg );
-        cairo_surface_destroy( cairo_surface_svg );
-    }
-    else
+        if (suffix == "svg")
+        {
+            auto cairo_surface_svg = cairo_svg_surface_create(outPath.c_str(), doc->Width(), doc->Height());
+            auto cairo_svg = cairo_create( cairo_surface_svg );
+            cairo_set_source_surface( cairo_svg, cairoSurface, 0, 0 );
+            cairo_paint( cairo_svg );
+            cairo_show_page( cairo_svg );
+            cairo_destroy( cairo_svg );
+            cairo_surface_destroy( cairo_surface_svg );
+        }
+        else
 #endif
 #if CAIRO_HAS_XML_SURFACE
-    if (suffix == "xml")
-    {
-        auto cairo_dev = cairo_xml_create(argv[2]);
-        cairo_xml_for_recording_surface(cairo_dev, cairoSurface);
-    }
-    else
+        if (suffix == "xml")
+        {
+            auto cairo_dev = cairo_xml_create(outPath.c_str());
+            cairo_xml_for_recording_surface(cairo_dev, cairoSurface);
+            cairo_device_flush( cairo_dev );
+            cairo_device_finish( cairo_dev );
+            cairo_device_destroy( cairo_dev );
+        }
+        else
 #endif
 #ifndef CAIRO_HAS_SCRIPT_SURFACE
-        cairo_surface_write_to_png( cairoSurface, argv[2] );   
+            cairo_surface_write_to_png( cairoSurface, outPath.c_str() );   
 #else
-    if (suffix == "png")
-        cairo_surface_write_to_png( cairoSurface, argv[2] );   
-    else
-    {
-        auto cairo_dev = cairo_script_create(argv[2]);
-        cairo_script_from_recording_surface(cairo_dev, cairoSurface);
-    }
+        if (suffix == "png")
+            cairo_surface_write_to_png( cairoSurface, outPath.c_str() );   
+        else
+        {
+            auto cairo_dev = cairo_script_create(outPath.c_str());
+            cairo_script_from_recording_surface(cairo_dev, cairoSurface);
+            cairo_device_flush( cairo_dev );
+            cairo_device_finish( cairo_dev );
+            cairo_device_destroy( cairo_dev );
+        }
 #endif
 
-    cairo_destroy( cairoContext );
-    cairo_surface_finish( cairoSurface );
-    cairo_surface_destroy( cairoSurface );
-    
+        cairo_destroy( cairoContext );
+        cairo_surface_finish( cairoSurface );
+        cairo_surface_destroy( cairoSurface );
+    }
+
+    doc.reset();
+    renderer.reset();
+
     return 0;
 }
