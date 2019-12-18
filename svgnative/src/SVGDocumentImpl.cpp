44cdc00ef8e51b295b51356bf5b02070706b3189
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2019-12-18 01:42:03 +0000

    negative rx or ry should be ignored

--- svgnative/src/SVGDocumentImpl.cpp
+++ svgnative/src/SVGDocumentImpl.cpp
@@ -498,8 +498,8 @@ std::unique_ptr<Path> SVGDocumentImpl::ParseShape(XMLNode* child)
             ry = 0;
         }
 
-        rx = std::min(rx, width / 2.0f);
-        ry = std::min(ry, height / 2.0f);
+        rx = std::max(0, std::min(rx, width / 2.0f));
+        ry = std::max(0, std::min(ry, height / 2.0f));
 
         auto path = mRenderer->CreatePath();
         if (rx == 0 && ry == 0)
