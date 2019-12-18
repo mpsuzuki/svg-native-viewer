9f087458dfe1a4f7e1bc97a80cfee6b680a3ec1d
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2019-12-18 03:09:58 +0000

    fix a type mismatch

--- svgnative/src/SVGDocumentImpl.cpp
+++ svgnative/src/SVGDocumentImpl.cpp
@@ -498,8 +498,8 @@ std::unique_ptr<Path> SVGDocumentImpl::ParseShape(XMLNode* child)
             ry = 0;
         }
 
-        rx = std::max(0, std::min(rx, width / 2.0f));
-        ry = std::max(0, std::min(ry, height / 2.0f));
+        rx = std::max(0.0f, std::min(rx, width / 2.0f));
+        ry = std::max(0.0f, std::min(ry, height / 2.0f));
 
         auto path = mRenderer->CreatePath();
         if (rx == 0 && ry == 0)
